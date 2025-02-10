6b88e473d7a7eba68869a368d5983648e5671edd
locwa <94048557+locwa@users.noreply.github.com>
2025-02-10 11:55:28 +0800

    added app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>{{ $title }}</title>
+</head>
+<body class="bg-gray-100 dark:bg-[#0f0f0f] text-[#0f0f0f] dark:text-gray-100 ">
+    @include('layouts.navigation')
+    {{ $slot }}
+</body>
+</html>
