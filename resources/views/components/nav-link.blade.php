98ef237fc7e1d0ff45b1957af92011a22038d7fc
locwa <94048557+locwa@users.noreply.github.com>
2025-02-10 12:58:33 +0800

    added navbar

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,1 @@
+<a {{ $attributes->merge(['class' => 'text-[#0f0f0f] dark:text-gray-100 hover:text-gray-500 hover:cursor-pointer text-lg']) }}> {{ $slot }}</a>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,12 @@
+<nav class="py-4 px-2 md:px-10 flex justify-between items-center">
+    <a href="./" class="flex">
+        <x-logos.crest class="h-auto w-16 mr-4"></x-logos.crest>
+        <x-logos.logo class="h-auto w-48"></x-logos.logo>
+    </a>
+    <div class="hidden md:flex gap-10 ">
+        <x-nav-link>Inventory</x-nav-link>
+        <x-nav-link>Services</x-nav-link>
+        <x-nav-link>About</x-nav-link>
+        <x-nav-link>Contact</x-nav-link>
+    </div>
+</nav>
