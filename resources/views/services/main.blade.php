97ec01c8ba67c93b431690acffcfca32f38d0f53
locwa <94048557+locwa@users.noreply.github.com>
2025-02-12 07:30:08 +0800

    made headers semibold

--- resources/views/services/main.blade.php
+++ resources/views/services/main.blade.php
@@ -12,7 +12,7 @@
     <div class="flex gap-10 mt-20 px-24 justify-between items-center">
         <div class="w-4/12">
             <div class="flex justify-between items-center">
-                <h1 class="text-3xl w-5/12">Storage</h1>
+                <h1 class="text-3xl w-5/12 font-semibold">Storage</h1>
                 <x-primary-button size="md">See More</x-primary-button>
             </div>
             <p class="py-5">Custom storage options for enthusiasts looking for personalized service.</p>
@@ -24,7 +24,7 @@
         <img class="w-7/12 h-[70vh] object-fit-cover" src="./storage/service_photos/main-1.webp" alt="car part">
         <div class="w-4/12">
             <div class="flex justify-between items-center">
-                <h1 class="text-3xl w-5/12">Restoration & Service</h1>
+                <h1 class="text-3xl w-5/12 font-semibold">Restoration & Service</h1>
                 <x-primary-button size="md">See More</x-primary-button>
             </div>
             <p class="py-5">Our technicians and artisans are committed to automotive excellence.</p>
@@ -34,7 +34,7 @@
     <div class="flex gap-10 mt-7 px-24 justify-between items-center">
         <div class="w-4/12">
             <div class="flex justify-between items-center">
-                <h1 class="text-3xl w-5/12">Sales</h1>
+                <h1 class="text-3xl w-5/12 font-semibold">Sales</h1>
                 <x-primary-button size="md">See More</x-primary-button>
             </div>
             <p class="py-5">Our well-networked and well-informed team has access to the most exceptional examples in the world.</p>
@@ -46,7 +46,7 @@
         <img class="w-7/12 h-[70vh] object-fit-cover" src="./storage/service_photos/main-3.webp" alt="car part">
         <div class="w-4/12">
             <div class="flex justify-between items-center">
-                <h1 class="text-3xl w-5/12">Collection Management</h1>
+                <h1 class="text-3xl w-5/12 font-semibold">Collection Management</h1>
                 <x-primary-button size="md">See More</x-primary-button>
             </div>
             <p class="py-5">Building collection wish-lists, documenting provenance and determining value.</p>
