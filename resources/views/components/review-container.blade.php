ee05ea906654e2015888887897781220bd7a348c
locwa <94048557+locwa@users.noreply.github.com>
2025-02-11 10:04:36 +0800

    added review section

---
+++ resources/views/components/review-container.blade.php
@@ -0,0 +1,8 @@
+@props([
+    'reviewerName',
+])
+
+<div class="w-80 h-64 px-8 py-4 flex flex-wrap content-between justify-center border-r-[1px] border-black">
+    <p class="text-lg">{{ $slot }}</p>
+    <p class="text-lg font-light">{{ $reviewerName }}</p>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,18 +21,18 @@
 
     <hr class="border-black dark:border-gray-100 mx-32">
 
-    <div class="my-20 flex flex-col items-center">
+    <div class="my-20 md:flex flex-col items-center">
         <h1 class="text-5xl tracking-wider mb-20">FEATURED GALLERY</h1>
-        <div class="flex h-[75vh] w-full gap-10 justify-center">
+        <div class="md:flex h-[75vh] w-full gap-10 justify-center">
             <img src="./storage/home_photos/collage-0.webp" alt="car">
             <div class="h-full flex gap-2">
                 <div class="flex gap-10 w-full">
                     <div class="h-[70vh] w-[30vw]">
-                        <img class="h-3/5 w-full mb-10 object-cover" src="./storage/home_photos/collage-1.webp" alt="car">
+                        <img class="h-3/5 w-full mb-11 object-cover" src="./storage/home_photos/collage-1.webp" alt="car">
                         <img class="h-2/5 w-full object-cover" src="./storage/home_photos/collage-2.webp" alt="car">
                     </div>
                     <div class="h-[70vh]">
-                        <img class="h-3/5 w-full mb-10 object-cover" src="./storage/home_photos/collage-3.webp" alt="car">
+                        <img class="h-3/5 w-full mb-11 object-cover" src="./storage/home_photos/collage-3.webp" alt="car">
                         <img class="h-2/5 w-full object-cover" src="./storage/home_photos/collage-4.webp" alt="car">
                     </div>
                 </div>
@@ -40,6 +40,25 @@
         </div>
     </div>
 
-    <hr class="border-black dark:border-gray-100 mx-32">
+    <div class="py-20 bg-black dark:bg-gray-100 text-gray-100 dark:text-black text-center">
+        <h1 class="text-5xl pb-14">WHAT OUR CLIENTS SAY ABOUT US</h1>
+        <div class="flex justify-center">
+            <x-review-container reviewerName="- Michael Rossi">
+                "The Vault's exotic car selection is unmatched!  Rare finds and knowledgeable staff make it a must-visit for any enthusiast."
+            </x-review-container>
+            <x-review-container reviewerName="- Isabela Rodriguez">
+                "Buying my McLaren from The Vault was a dream.  Exceptional service and a personalized experience made it truly special."
+            </x-review-container>
+            <x-review-container reviewerName="- David Chen">
+                "The Vault's service department is top-notch.  Expert technicians and efficient repairs keep my Lamborghini running perfectly."
+            </x-review-container>
+            <x-review-container reviewerName="- Emily Carter">
+                "The Vault's showroom is stunning.  A true temple of automotive art, creating an unforgettable experience."
+            </x-review-container>
+            <x-review-container reviewerName="- Robert Johnson">
+                "I appreciated The Vault's transparency.  Honest pricing and a straightforward process made my purchase stress-free."
+            </x-review-container>
+        </div>
+    </div>
 
 </x-app-layout>
