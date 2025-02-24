1b02f60d21bab465cb4e089d168ddd64caf674f0
locwa <94048557+locwa@users.noreply.github.com>
2025-02-24 23:02:20 +0800

    added blocking

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -5,6 +5,23 @@
         <h1 class="text-[5rem]">Inventory</h1>
     </div>
 
+    <div class="px-24 text-center">
+        <div class="flex w-full gap-10">
+            <div class="grow-1 w-1/3 h-[90vh] justify-start dark:bg-gray-800 my-5">
 
+            </div>
+            <div class="grow-4 flex flex-wrap flex-row w-full">
+                    @foreach($inventory as $inv)
+                        <div class="w-80 h-96 dark:bg-gray-800 m-5">
+
+                        </div>
+                    @endforeach
+
+            </div>
+        </div>
+
+        {{ $inventory->links() }}
+
+    </div>
 
 </x-app-layout>
