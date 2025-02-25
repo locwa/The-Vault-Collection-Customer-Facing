4d219967af31232e957bf89f5ec08ad16f977166
locwa <94048557+locwa@users.noreply.github.com>
2025-02-25 19:15:03 +0800

    added filter panel

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -8,9 +8,26 @@ use App\Models\Inventory;
 class InventoryController extends Controller
 {
     //
-    public function viewAll(Request $request)
-    {
-        $output = Inventory::paginate(12);
-        return view('inventory', ['inventory' => $output]);
+    public function viewAll(Request $request){
+
+        $inventory = [];
+        request()->query('car-id') ? array_push($inventory, ['id', request()->query('car-id')]) : "";
+        request()->query('make') ? array_push($inventory, ['make', request()->query('make')]) : "";
+        request()->query('model') ? array_push($inventory, ['model', request()->query('model')]) : "";
+        request()->query('year') ? array_push($inventory, ['year', request()->query('year')]) : "";
+        request()->query('status') ? array_push($inventory, ['status', 1]) : array_push($inventory, ['status', 0]);
+
+
+        $makes = Inventory::select('make')->distinct()->orderBy('make', 'ASC')->get();
+        $models = Inventory::select('model')->distinct()->get();
+
+        if (count($inventory) == 0){
+            $inventory = Inventory::paginate(12);
+
+        } else {
+            $inventory = Inventory::where($inventory)->paginate(12);
+        }
+
+        return view('inventory', ['inventory' => $inventory, 'makes' => $makes, 'models' => $models]);
     }
 }
--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -7,9 +7,44 @@
 
     <div class="px-24 text-center">
         <div class="flex w-full gap-10">
-            <div class="grow-1 w-1/3 h-[90vh] justify-start bg-gray-300 dark:bg-[#1f1f1f] my-5 rounded-[10px]">
-
+            <div class="grow-1 w-1/3 h-[50vh] justify-start bg-gray-300 dark:bg-[#1f1f1f] rounded-[10px] p-6 text-left">
+                <a href="{{ route('inventory') }}" class="flex justify-center">
+                    <x-primary-button class="px-20 py-3 mb-5 w-full justify-center">Reset Search</x-primary-button>
+                </a>
+                <form method="GET" action="{{ route('inventory') }}">
+                    <div class="flex flex-col mb-2">
+                        <label for="status">Status</label>
+                        <select name="status" id="status" class="text-black px-2 py-2 rounded-[5px]">
+                            <option value="0" selected>Available</option>
+                            <option value="1" {{ request()->query('status') ? "selected" : "" }}>Sold</option>
+                        </select>
+                    </div>
+                    <div class="flex flex-col mb-2">
+                        <label for="id" class="text-[#0f0f0f] dark:text-gray-100 font-semibold">Year:</label>
+                        <input type="text" name="year" id="year" class="text-black p-2 rounded-[5px]" value="{{ request()->query('year') }}">
+                    </div>
+                    <div class="flex flex-col mb-2">
+                        <label for="make">Make</label>
+                        <select name="make" id="make" class="text-black px-2 py-2 rounded-[5px]">
+                            <option value="" selected disabled>Select One</option>
+                            @foreach($makes as $make)
+                                <option value="{{ $make->make }}" {{$make->make == request()->query('make') ? "selected" : "" }}>{{ $make->make }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="flex flex-col mb-2">
+                        <label for="model">Model</label>
+                        <select name="model" id="model" class="text-black px-2 py-2 rounded-[5px]">
+                            <option value="" selected disabled>Select One</option>
+                            @foreach($models as $model)
+                                <option value="{{ $model->model }}" {{$model->model == request()->query('model') ? "selected" : "" }}>{{ $model->model }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <x-primary-button class="my-2">Search</x-primary-button>
+                </form>
             </div>
+
             <div class="grow-4 flex flex-wrap flex-row w-full mb-8">
                     @foreach($inventory as $inv)
                         <a href="" class="w-[22rem] h-[43vh] m-2 flex flex-col flex-wrap justify-between">
