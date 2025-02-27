f91712358316246534576fbbc2acdad76f71fd52
locwa <94048557+locwa@users.noreply.github.com>
2025-02-27 09:42:34 +0800

    made home page responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,21 @@
             <h1 class="text-5xl/[3.5rem] max-w-[60vw]">The Vault. Purveyors of some of the most exclusive cars in the planet.</h1>
         </x-slot:heading>
         <x-slot:details>
-            <p class="text-md font-light max-w-[15vw] text-right">Since our inception, we have sourced, procured, and sold over 500 of the most exclusive supercars and hypercars in the entire world.</p>
+            <p class="hidden md:block text-md font-light max-w-[15vw] text-right">Since our inception, we have sourced, procured, and sold over 500 of the most exclusive supercars and hypercars in the entire world.</p>
         </x-slot:details>
     </x-image-carousel>
 
-    <hr class="border-black dark:border-gray-100 mx-32">
+    <hr class="border-black dark:border-gray-100 mx-6 md:mx-32">
 
-    <div class="mx-72 my-20 text-center">
+    <div class="mx-5 md:mx-72 my-20 text-center">
         <p class="text-3xl">"In a segment often characterized by inflated egos and high-pressure tactics, The Vault Collection offers a more refined and customer-centric approach.  They understand that purchasing an exotic car is more than just a transaction; it's the realization of a dream.  Apex facilitates that dream with a level of polish and professionalism that earns them a strong recommendation.  For those navigating the often-turbulent waters of exotic car acquisition, The Vault Collection provides a welcome port in the storm".</p>
         <p class="text-2xl font-light mt-8">Arthur Sterling</p>
     </div>
 
-    <hr class="border-black dark:border-gray-100 mx-32">
+    <hr class="border-black dark:border-gray-100 mx-6 md:mx-32">
 
-    <div class="my-20 md:flex flex-col items-center">
-        <h1 class="text-[4rem] tracking-wider font-semibold mb-20">FEATURED GALLERY</h1>
+    <div class="my-20 hidden md:flex flex-col items-center">
+        <h1 class="text-[4rem] tracking-wider font-semibold mb-20 text-center">FEATURED GALLERY</h1>
         <div class="md:flex h-[75vh] w-full gap-10 justify-center">
             <img src="./images/home_photos/collage-0.jpg" alt="car">
             <div class="h-full flex gap-2">
@@ -40,7 +40,7 @@
         </div>
     </div>
 
-    <div class="py-20 bg-black dark:bg-gray-100 text-gray-100 dark:text-black text-center">
+    <div class="px-5 py-20 h-[45rem] lg:h-[35rem] bg-black dark:bg-gray-100 text-gray-100 dark:text-black text-center">
         <h1 class="text-5xl pb-14 font-semibold">WHAT OUR CLIENTS SAY ABOUT US</h1>
         <div class="flex justify-center">
             <x-review-container reviewerName="- Michael Rossi">
@@ -49,13 +49,13 @@
             <x-review-container reviewerName="- Isabela Rodriguez">
                 "Buying my McLaren from The Vault was a dream.  Exceptional service and a personalized experience made it truly special."
             </x-review-container>
-            <x-review-container reviewerName="- David Chen">
+            <x-review-container reviewerName="- David Chen" class="hidden sm:flex">
                 "The Vault's service department is top-notch.  Expert technicians and efficient repairs keep my Lamborghini running perfectly."
             </x-review-container>
-            <x-review-container reviewerName="- Emily Carter">
+            <x-review-container reviewerName="- Emily Carter" class="hidden md:flex">
                 "The Vault's showroom is stunning.  A true temple of automotive art, creating an unforgettable experience."
             </x-review-container>
-            <x-review-container reviewerName="- Robert Johnson">
+            <x-review-container reviewerName="- Robert Johnson" class="hidden lg:flex">
                 "I appreciated The Vault's transparency.  Honest pricing and a straightforward process made my purchase stress-free."
             </x-review-container>
         </div>
