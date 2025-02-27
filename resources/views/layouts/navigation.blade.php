9654577f981897d872c8616fa54422efcf3be3a5
locwa <94048557+locwa@users.noreply.github.com>
2025-02-27 10:10:13 +0800

    made nav bar stick to the top

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,5 @@
-<nav class="py-4 px-2 md:px-10 flex justify-between items-center">
+<nav class="py-4 px-2 md:px-10 flex justify-between items-center sticky top-0 bg-gray-100 dark:bg-[#0f0f0f]">
+
     <a href="{{route('home')}}" class="flex">
         <x-logos.crest class="h-auto w-16 mr-4"></x-logos.crest>
         <x-logos.logo class="h-auto w-48"></x-logos.logo>
