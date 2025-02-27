910b026c11d94451336ef37ed4fec358650c0b3b
locwa <94048557+locwa@users.noreply.github.com>
2025-02-27 10:15:37 +0800

    added prop for text size in nav-link

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,5 @@
-<a {{ $attributes->merge(['class' => 'text-[#0f0f0f] dark:text-gray-100 hover:text-gray-500 hover:cursor-pointer text-lg']) }}> {{ $slot }}</a>
+@props([
+    'textSize' => 'lg',
+])
+
+<a {{ $attributes->merge(['class' => 'text-[#0f0f0f] dark:text-gray-100 hover:text-gray-500 hover:cursor-pointer text-'.$textSize.']) }}> {{ $slot }}</a>
