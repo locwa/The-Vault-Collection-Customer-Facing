8a7eb4cb3403edce34cd3b73ecae28df0255f387
locwa <94048557+locwa@users.noreply.github.com>
2025-02-27 09:42:10 +0800

    made component accept attributes

--- resources/views/components/review-container.blade.php
+++ resources/views/components/review-container.blade.php
@@ -2,7 +2,7 @@
     'reviewerName',
 ])
 
-<div class="w-80 h-64 px-8 py-4 flex flex-wrap content-between justify-center border-r-[1px] border-black">
+<div {{ $attributes->merge(['class' => 'w-80 h-[40vh] md:h-64 px-8 py-4 flex flex-wrap content-between justify-center border-r-[1px] border-black']) }}>
     <p class="text-lg">{{ $slot }}</p>
     <p class="text-lg font-light">{{ $reviewerName }}</p>
 </div>
