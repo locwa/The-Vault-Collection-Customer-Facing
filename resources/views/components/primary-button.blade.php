4afb857803c1c01cc44f65a4aea604a4cc8dc31d
locwa <94048557+locwa@users.noreply.github.com>
2025-02-28 23:59:53 +0800

    added border for inverted

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -6,7 +6,7 @@
 @php
     $colors = "";
     if ($inverted){
-        $colors = 'dark:bg-[#0f0f0f] bg-gray-100 dark:text-white text-[#0f0f0f] dark:hover:bg-gray-700 hover:bg-white dark:focus:bg-gray-700 focus:bg-white dark:active:bg-gray-900 active:bg-gray-300 dark:focus:ring-offset-2 focus:ring-offset-gray-800';
+        $colors = 'dark:bg-[#0f0f0f] bg-gray-100 dark:text-white text-[#0f0f0f] dark:hover:bg-gray-700 hover:bg-white dark:focus:bg-gray-700 focus:bg-white dark:active:bg-gray-900 active:bg-gray-300 dark:focus:ring-offset-2 focus:ring-offset-gray-800 border-white';
     } else {
         $colors = 'bg-[#0f0f0f] dark:bg-gray-100 text-white dark:text-[#0f0f0f] hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:ring-offset-2 dark:focus:ring-offset-gray-800';
     }
