a4483fe840351f039edacf702c1880377c1de669
locwa <94048557+locwa@users.noreply.github.com>
2025-02-28 23:29:42 +0800

    initial restyling of inventory page

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot:title>Inventory</x-slot:title>
 
-    <div class="flex justify-between items-center px-24 py-10">
+    <div class="flex items-center justify-center md:justify-start md:px-24 md:py-10">
         <h1 class="text-[5rem]">Inventory</h1>
     </div>
 
-    <div class="px-24 text-center">
+    <div class="px-4 md:px-24 text-center">
         <div class="flex w-full gap-10">
-            <div class="grow-1 w-1/3 h-[50vh] justify-start bg-gray-300 dark:bg-[#1f1f1f] rounded-[10px] p-6 text-left">
+            <div class="hidden md:flex flex-col grow-1 w-1/3 h-[50vh] justify-start bg-gray-300 dark:bg-[#1f1f1f] rounded-[10px] p-6 text-left">
                 <a href="{{ route('inventory') }}" class="flex justify-center">
                     <x-primary-button class="px-20 py-3 mb-5 w-full justify-center">Reset Search</x-primary-button>
                 </a>
@@ -46,9 +46,9 @@
             </div>
 
             @if($inventory->total() > 0)
-                <div class="grow-4 flex flex-wrap flex-row w-full mb-12">
+                <div class="grow-4 flex flex-wrap flex-row w-full mb-12 md:justify-start justify-center">
                     @foreach($inventory as $inv)
-                        <a href="{{ route('view-car', $inv->id) }}" class="w-[22rem] h-[43vh] m-2 flex flex-col flex-wrap justify-between hover:bg-gray-300 dark:hover:bg-[#1f1f1f] rounded-[5px]">
+                        <a href="{{ route('view-car', $inv->id) }}" class="w-[22rem] h-[43rem] m-2 flex flex-col flex-wrap justify-between hover:bg-gray-300 dark:hover:bg-[#1f1f1f] rounded-[5px]">
                             <div>
                                 <img src="./storage/cars/{{$inv->photo_header}}0.jpg" alt="" srcset="">
                                 <h4 class="text-2xl my-2 px-2">{{sprintf("%u %s %s", $inv->year, $inv->make, $inv->model )}}</h4>
