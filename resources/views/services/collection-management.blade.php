c4158d5e3c2847e94cec2eb1e9d17c048356285b
locwa <94048557+locwa@users.noreply.github.com>
2025-02-28 22:53:35 +0800

    made collection management page responsive

--- resources/views/services/collection-management.blade.php
+++ resources/views/services/collection-management.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
     <x-slot:title>Collection Management</x-slot:title>
 
-    <div class="px-20 py-7">
+    <div class="px-4 md:px-20 py-7">
         <div class="w-full">
-            <div class="flex justify-between items-center">
+            <div class="mb-5 md:mb-0 md:flex justify-between items-center">
                 <h1 class="text-[4rem] font-semibold">Collection Management</h1>
                 <x-primary-button size="md">Learn More</x-primary-button>
             </div>
@@ -13,27 +13,27 @@
                 <p class="text-sm text-gray-400">Accurate Valuation</p>
             </div>
         </div>
-        <div class="w-full flex justify-end pt-3">
-            <p class="w-8/12 text-right">
+        <div class="w-full flex md:justify-end pt-3">
+            <p class="md:w-8/12 md:text-right">
                 The Vault’s Collection Management service expertly assists clients in building and maintaining their unique collections of vintage and supercars. Our dedicated team works closely with collectors to create tailored wish-lists, document provenance, and accurately determine the value of each vehicle. We focus on positioning these treasured cars as both valuable assets and passionate pursuits, ensuring every collection is as distinctive and remarkable as its owner.p>
         </div>
 
-        <div class="pt-8 pb-20 flex gap-20 justify-between items-end">
-            <div class="w-4/12">
+        <div class="pt-8 pb-20 md:flex gap-20 justify-between items-end">
+            <div class="md:w-4/12">
                 <h1 class="text-3xl font-semibold">The Vault's Collection Management Services</h1>
-                <p class="pt-6">The Vault specializes in providing high-quality collection management services for vintage and supercar enthusiasts. Our dedicated team works closely with collectors to build and maintain unique, high-value collections that reflect both passion and preserve history for generations.</p>
+                <p class="mb-5 md:mb-0 pt-6">The Vault specializes in providing high-quality collection management services for vintage and supercar enthusiasts. Our dedicated team works closely with collectors to build and maintain unique, high-value collections that reflect both passion and preserve history for generations.</p>
             </div>
-            <img class="w-7/12 h-[70vh] object-fit-cover" src="../images/service_photos/collection-management-0.webp" alt="dsfasd">
+            <img class="md:w-7/12 h-[70vh] object-fit-cover" src="../images/service_photos/collection-management-0.webp" alt="dsfasd">
         </div>
 
-        <hr class="border-black dark:border-gray-100 mx-32">
+        <hr class="border-black dark:border-gray-100 md:mx-32">
 
-        <div class="flex justify-between py-12 px-9">
-            <h1 class="text-3xl">Why Choose Our Collection Management Services?</h1>
-            <p class="font-light text-xl w-6/12">Our approach to collection management is personalized and meticulous. We understand that each collection is a unique reflection of its owner, and we strive to provide services that cater to your specific needs and goals. Here’s what we offer:</p>
+        <div class="md:flex justify-between py-12 px-9">
+            <h1 class="text-3xl mb-5 md:mb-0">Why Choose Our Collection Management Services?</h1>
+            <p class="font-light text-xl md:w-6/12">Our approach to collection management is personalized and meticulous. We understand that each collection is a unique reflection of its owner, and we strive to provide services that cater to your specific needs and goals. Here’s what we offer:</p>
         </div>
-        <div class="py-16 flex justify-between">
-            <div class="w-[25rem]">
+        <div class="py-16 md:flex justify-between">
+            <div class="w-full md:w-[25rem]">
                 <img class="max-w-[25rem] h-[20rem] object-fit-cover" src="../images/service_photos/collection-management-1.webp" alt="photo">
                 <div class="py-5">
                     <h3 class="text-2xl font-semibold">Tailored Wish-Lists</h3>
